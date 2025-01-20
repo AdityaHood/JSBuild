fc64388a8d92a1304c416850ae1ded2167c60cfe
AdityaHood <adityahood13@gmail.com>
2025-01-20 23:08:27 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,10 @@
+const state = {
+  taskList: [],
+};
+
+//DOM operations here
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+console.log(taskContents);
+console.log(taskModal);
